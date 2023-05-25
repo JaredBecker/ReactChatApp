9e866cfdebbb8002a9ede68faa205eb68620acb3
Jared Becker <jaredbecker46@gmail.com>
2023-05-25 14:35:11 +0200

    - Setup mongo DB connection - Moved React app 1 folder up

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+// Initializing the application
+const app = express();
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+/**
+ * Connects to the MongoDB
+ * useUnifiedTopology -> MongoDB driver's new connection management engine
+ */
+mongoose.connect(process.env.MONGO_URL, {
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('DB Connection Successful');
+})
+.catch((err) => {
+    console.log(err.message);
+})
+
+// Start the server on the port setup in our env file
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server Started on PORT: ${process.env.PORT}`);
+})
